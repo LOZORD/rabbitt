e199e866a320373bf8a90c80650c5f659b7659de
LOZORD <ljrudberg@gmail.com>
2014-02-28 21:00:28 -0600

    Got jQuery working. Added array of e elements and colors. Now time to beautify.

--- script.js
+++ script.js
@@ -2,61 +2,94 @@ $(document).ready(
 
 	function()
 	{
+		var eArr = 	[
+						[$("#e0"), $("#e1"), $("#e2"), $("#e3")],
+						[$("#e4"), $("#e5"), $("#e6"), $("#e7")],
+						[$("#e8"), $("#e9"), $("#e10"), $("#e11")],
+						[$("#e12"), $("#e13"), $("#e14"), $("#e15")]
+					];
+		var colors = ["red","green","blue","yellow"];
 
-		var binString = $("#screen").text();
+		var myScreen = $("#screen");
+
+		//console.log(binString);
 
 		$('#button0').click(
 			function()
 			{
-				if (binString.length < 6)
-					binString += "0";
-				else
-					binString = binString.substr(1) + "0";
-				// console.log("added 0");
+				//var binString = myScreen.text();
 
-				// if(binString.length === 6)
-				// 	$("#screen").css("color","green");
+				if (myScreen.text().length < 6)
+					myScreen.append("0");
+				else
+					myScreen.text(myScreen.text().substr(1) + "0");
+
+				//$("#screen").val(binString);
+
+				if (myScreen.text().length === 6)
+					$("#button_enter").css("background-color","darkgray");
+				
+				//console.log(binString);
 			}
 		);
 
 		$('#button1').click(
 			function()
 			{
-				if (binString.length < 6)
-					binString += "1";
+				if (myScreen.text().length < 6)
+					myScreen.append("1");
 				else
-					binString = binString.substr(1) + "1";
-				
-				// console.log("added 1");
+					myScreen.text(myScreen.text().substr(1) + "1");
 
-				// if(binString.length === 6)
-				// 	$("#screen").css("color","green");
+				//$("#screen").val(binString);
 
+				if (myScreen.text().length === 6)
+					$("#button_enter").css("background-color","darkgray");
+
+				//console.log(binString);
 			}
 		);
 
-		// if(binString.length === 6)
-		// {
-		// 	$("#screen").css("color","green");
-		// }
 
 		$("#button_enter").click(
 			function()
 			{
-				console.log(binString);
-				updateBox(binString);
-				binString = "";
+				//console.log("pizza");
+
+				if (myScreen.text().length === 6)
+				{
+					//console.log(binString);
+					updateBox(myScreen.text());
+
+					myScreen.text("");
+
+					$("#button_enter").css("background-color","lightgray")
+
+					//$("#screen").val(binString);
+				}
 			}
 		);
 
-		$("#screen").text(binString);
+		//$("#screen").val(binString);
 
 
 		function updateBox (str)
 		{
-			//$("#screen").text(str);
-			console.log(str);
-			///UPDATE COLOR BOXES HERE, AKA PARSE!
+			// //$("#screen").text(str);
+			// console.log("HOLA");
+			// ///UPDATE COLOR BOXES HERE, AKA PARSE!
+
+			var colNum = parseInt(str.substr(0,2), 2);
+			var rowNum = parseInt(str.substr(2,2), 2);
+			var aColor = parseInt(str.substr(4,2), 2);
+
+			eArr[rowNum][colNum].css("background-color", colors[aColor]);
+
+
+
+
+			//console.log(str);
+
 		}
 		
 
