500ba467c15238c59d24ef896b7305c3cc1b7750
LOZORD <ljrudberg@gmail.com>
2014-04-29 18:26:41 -0500

    Starting new version and refactoring

--- script.js
+++ script.js
@@ -2,14 +2,25 @@ $(document).ready(
 
 
 	/* TODO
-		-- make code less janky i.e. refactor
-		-- make row/col conflict and weird html less janky
+	-- make code less janky i.e. refactor
+	-- make row/col conflict and weird html less janky
+    -- add x button for overlays
+    xx bigger button font
+    xx change background
+    -- feedback for partial strings
+    -- add indication to screen to show length of required string
+    -- flash text on save and load
+    xx add backspace compatibility
+    -- give examples
+    -- hammer.js for tablet compatibility
+    -- make it into a puzzle toy
+    xx fix enter button
+    ?? make more dynamic by using logs of eArr for bit sizing
 	*/
 
 	function()
 	{
 		//the array of grid elements
-
 		var eArr = 	[
 						[$("#e0"), $("#e1"), $("#e2"), $("#e3")],
 						[$("#e4"), $("#e5"), $("#e6"), $("#e7")],
@@ -24,14 +35,10 @@ $(document).ready(
 
 		const numColBits = 2; // = log2(eArr.length)
 
+        //dimensions of eArr
 		const rowLength = Math.pow(2, numRowBits);
 
 		const colLength = Math.pow(2, numColBits);
-
-		//must be a positive number divisible by 3
-		//const numClrBits = 3;
-
-		//const eachClrBit = numClrBits/3;
 
 		const eachClrBit = 2;
 
@@ -45,13 +52,26 @@ $(document).ready(
 		const zeroKey = 48;
 		const oneKey = 49;
 		const enterKey = 13;
+        const bckspKey = 8;
+
+        //code names
+        const enterCode = 'entr';
+        const resetCode = 'rset';
+        const bckspCode = 'back';
+        const printCode = 'prnt';
+        const _saveCode = 'save';
+        const _loadCode = 'load';
+
+        const code0 = '0';
+        const code1 = '1';
+
 
 		//slideover appearance booleans
 		var guideGone = false;
 		var holdGuide = false;
 
 		//XXX needed?
-		var oneSubmit = false;
+		// var oneSubmit = false;
 
 		$("#amntColors").text(Math.pow(2, numClrBits));
 		$("#binStrLen").text(binStringLengthMax);
@@ -70,25 +90,27 @@ $(document).ready(
 				{
 
 					//get the particular event
+                    //remember that these are sent back LOWERCASE!!!
 					var myTarget = getEventTarget(e);
 
-					if (myTarget == null)
+					if (myTarget === null)
 						return;
-					else if (myTarget === 'enter')
+					else if (myTarget === enterCode)
 						submitScreen();
 					//the user entered a one or a zero
 					//either via kb or clicking the buttons
-					else if (myTarget === '0' || myTarget === '1')
+                    else if (myTarget === bckspCode)
+                        doBackspace(e);
+					else if (myTarget === code0 || myTarget === code1)
 						updateScreen(myTarget);
-					else if (myTarget === 'reset')
+					else if (myTarget === resetCode)
 						resetScreen();
 					//TODO make own function!!!
-					else if (myTarget === 'print')
+					else if (myTarget === printCode)
 					{
 						var output = genHTMLScreen();
 
 						$('#guide').text(output);
-						//$('#guideMsg').text('HELLO');
 
 
 						$('#guide').prepend('<span style="color:gray;">---<br/><br/>Your RABBITT printout...<br/>\
@@ -107,20 +129,19 @@ $(document).ready(
 
 					}
 
-					else if (myTarget === 'save')
+					else if (myTarget === _saveCode)
 					{
 						saveState();
 					}
-					else if (myTarget === 'load')
+					else if (myTarget === _loadCode)
 					{
 						loadState();
 					}
 
-					//XXX use?
+					//XXX use? TODO
 					myTarget = "#" + myTarget;
 
 				}
-				//console.log(e);
 			}
 		);
 
@@ -191,7 +212,7 @@ $(document).ready(
 		function setScreenElement(rowNum, colNum, color, time)
 		{
 			//set 750 ms as the default
-			if (typeof(time)==='undefined') time = 750;
+			if (typeof(time)=== 'undefined') time = 750;
 
 			eArr[rowNum][colNum].animate( {backgroundColor : color} , time );
 		}
@@ -224,6 +245,7 @@ $(document).ready(
 
 			var clrNumArr = myElement.css("background-color").split(',');
 
+            //remove the preceding "rgb", then get each assoc'd color
 			var red = clrNumArr[0].substr(3);
 			var grn = clrNumArr[1];
 			var blu = clrNumArr[2];
@@ -236,6 +258,23 @@ $(document).ready(
 		{
 			return (num * 255 / (Math.pow(2, eachClrBit) - 1));
 		}
+
+        function doBackspace(e)
+        {
+            //aha! this call on e stops the page from going backwards!
+            e.preventDefault();
+            
+            //get the screen text
+            var screenText = myScreen.text();
+            //get its length
+            var textLen = screenText.length;
+            //delete one character
+            var newText = screenText.substr(0, textLen - 1);
+            //update the text
+            myScreen.text(newText);
+
+            //myScreen.text(screenText.substr())
+        }
 	
 		//what to do when the user presses the enter button
 		function submitScreen()
@@ -251,7 +290,7 @@ $(document).ready(
 					//change the button back to the "inactive" color
 					$("#button_enter").css("background-color","lightgray");
 
-					oneSubmit = true;
+					// oneSubmit = true; XXX
 				}
 		}
 
@@ -259,14 +298,12 @@ $(document).ready(
 		function getEventTarget(e)
 		{
 
-			//console.log(e)
-
 			if (e.type === 'keydown')
 			{
 				return determineKey(e);
 			}
 
-			//they must have clicked
+			//they must have clicked, so find the assoc'd div
 			else if (e.type === 'click' && (e.toElement.className.search('clickBtn') > 1))
 			{
 
@@ -274,17 +311,21 @@ $(document).ready(
 			}
 
 			else
+            {
 				return null;
+            }
 		}
 
 		function determineKey (e)
 		{
 			if (e.keyCode === zeroKey)
-				return '0';
+				return code0;
 			else if (e.keyCode === oneKey)
-				return '1';
+				return code1;
 			else if (e.keyCode === enterKey)
-				return 'enter';
+				return enterCode;
+            else if (e.keyCode === bckspKey)
+                return bckspCode;
 			else
 				return null;
 		}
@@ -295,6 +336,7 @@ $(document).ready(
 			{
 				for (var j = 0; j < colLength; j++)
 				{
+                    //animate fade back to the original red tile colors
 					eArr[i][j].animate({backgroundColor: "red"}, 750);
 				}
 			}
@@ -340,24 +382,12 @@ $(document).ready(
 				screen_data += '</div>\n';
 			}
 
-			//end the html file
+			//end the html file payload
 			screen_data += "</body></html>";
-
-			//console.log(screen_data);
-
-			//alert('Check your console!');
 
 			return screen_data;
 
 		}
-
-		//GARBAGE!
-		function download(filename, text)
-		{
-			alert('unimplemented!');
-		}
-
-
 
   		function colorizeScreenText()
   		{
@@ -372,7 +402,7 @@ $(document).ready(
 
 			var redAmnt = rgbNormalize(parseInt(rgbStr.substr(0, eachClrBit), 2));
 			var grnAmnt = rgbNormalize(parseInt(rgbStr.substr(eachClrBit, eachClrBit), 2));
-			var bluAmnt = rgbNormalize(parseInt(rgbStr.substr(2*eachClrBit), 2));
+			var bluAmnt = rgbNormalize(parseInt(rgbStr.substr(2 * eachClrBit), 2));
 
 			var toRGBString = "rgb(" + redAmnt + "," + grnAmnt + "," + bluAmnt + ")";
 
@@ -386,6 +416,7 @@ $(document).ready(
 
 
 			var eRow = parseInt(rowStr, 2);
+
 			var eCol = parseInt(colStr ,2);
 
 
@@ -401,13 +432,9 @@ $(document).ready(
 
   		}
 
-  		//TODO save func
+  		//save func
   		function saveState()
   		{
-  			//console.log('Are you attaching this functio to an event?');
-  			//console.log("SAVE THIS IP ADDR: " + userIpAddr);
-
-
   			var textToSave = '';
 
   			for (var row in eArr)
@@ -417,101 +444,52 @@ $(document).ready(
 
   				for (var col in eArr[row])
   				{
-  					//textToSave += ( eArr[col][row].css('backgroundColor') + ' , ');
-
-  					//console.log('r' + row + 'c' + col + '...' + eArr[col][row].css('backgroundColor'));
-
-  					//textToSave += ('[' + row + '][' +  col + ']' + eArr[col][row].css('backgroundColor') + ' | ');
-
   					textToSave += (eArr[col][row].css('backgroundColor') + '|')
-
   				}
 
   				//textToSave += '}';
   			}
 
-  			//textToSave += '>';
-
-  			//console.log("\n\n" + textToSave);
-
   			//WRITE TO THE COOKIE
   			//Save cookies until "the end of time"
   			docCookies.setItem('RABBITT_save_data', textToSave, new Date(0x7fffffff * 1e3));
 
-  			console.log('set cookie!')
 
 
   		}
 
-  		//TODO load function
+        //load func
   		function loadState()
   		{
-  			//console.log(docCookies.getItem('RABBITT_save_data'));
-
-  			console.log('loading cookie!');
 
   			var cookieFetchRes = docCookies.getItem('RABBITT_save_data');
-
-
 
   			//if the user hasn't previously saved their data
   			if (cookieFetchRes === null)
   				return;
 
-
-  			//var fetchString = new String(cookieFetchRes);
-
-  			//assuming we've recieved some type of cookie...
-
-  			//console.log(cookieFetchRes);
-
-  			// console.log(typeof cookieFetchRes);
-
-  			// console.log('gerald!');
-
   			cookieFetchRes.trim();
-
-
-  			// console.log(fetchString.search('|'));
-
-
-
-  			// if (!fetchString.contains('rgb'))
-  			// {
-  			// 		console.log('salty dog!');
-  			// 		return;
-  			// }
 
   			//assume the cookie is valid, decode the result
   			//limit splitting to the number of elements in eArr
   			var decodeArr = cookieFetchRes.split('|', rowLength * colLength);
 
-  			console.log('split cookie!');
-
-  			// for (var i in decodeArr)
-  			// 	console.log(decodeArr[i]);
-
+  			
   			var i = 0;
 
   			for (var row in eArr)
   			{
   				for (var col in eArr[row])
   				{
-  					//eArr[row][col].css('backgroundColor');
-  					//make code less redundant -- see updateBox func above
-					//eArr[col][row].animate({backgroundColor: decodeArr[i++]}, 750);
 					setScreenElement(col ,row, decodeArr[i++]);
-
   				}
   			}
-
-
-
 
   		}
 
 
-	}
+
+	} //end inclosing function
 
 );
 
